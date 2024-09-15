c79437ac0f2273b4605bd03c44df724696b86707
Adam-Kosciukiewicz <adamkosciukiewiczkontakt@gmail.com>
2024-09-15 20:02:49 +0200

    src change

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import Button from "../elements/Button";
+import image from "../assets/logo.png"
 
 const Naviagtion: React.FC = (): JSX.Element => {
     return (
         <div className='w-full h-[79px] flex flex-row items-center place-content-between lg:px-[120px] md:px-[100px] px-[30px] relative z-10'>
             <div>
-                <img src="./src/assets/logo.png" className='md:h-[32px] h-[24px]' />
+                <img src={image} className='md:h-[32px] h-[24px]' />
             </div>
             <div className='hidden flex-row gap-[23px] md:text-[16px] text-[14px] xs:flex'>
                 <a className='hover:text-mainDark text-black hover:opacity-80 hover:underline cursor-pointer' href="#gallery">Galeria zdjęć</a>
