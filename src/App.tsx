255493adca389a3b736b0032ad5d5e5dc4709205
Adam-Kosciukiewicz <adamkosciukiewiczkontakt@gmail.com>
2024-09-15 20:12:44 +0200

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -3,33 +3,40 @@ import Navigation from "./components/Navigation"
 import Baner from './components/Baner'
 import Gallery from './components/Gallery'
 import Footer from './components/Footer'
+import background from "./assets/background.png";
+import car1 from "./assets/slider-car1.png";
+import car2 from "./assets/slider-car2.png";
+import car3 from "./assets/slider-car3.png";
+import truck1 from "./assets/truck1.webp";
+import truck2 from "./assets/truck2.webp";
+import truck3 from "./assets/truck3.webp";
+
 
 const App: React.FC = (): JSX.Element => {
   const galleryImages = {
     passenger: [
-      "./src/assets/slider-car3.png",
-      "./src/assets/slider-car1.png",
-      "./src/assets/slider-car2.png",
-      "./src/assets/slider-car1.png",
-      "./src/assets/slider-car3.png",
-      "./src/assets/slider-car2.png",
-      "./src/assets/slider-car1.png",
-      "./src/assets/slider-car3.png",
-      "./src/assets/slider-car2.png",
+      car3,
+      car1,
+      car2,
+      car3,
+      car1,
+      car2,
+      car3,
+      car1,
     ],
     van: [
-      "./src/assets/truck1.webp",
-      "./src/assets/truck2.webp",
-      "./src/assets/truck3.webp",
-      "./src/assets/truck1.webp",
-      "./src/assets/truck2.webp",
-      "./src/assets/truck3.webp",
+      truck1,
+      truck2,
+      truck3,
+      truck1,
+      truck2,
+      truck3,
     ]
   };
 
   return (
     <div className='w-full bg-mainLight h-full bg-no-repeat bg-right-top font-RobotoFlex relative'>
-      <img src="./src/assets/background.png" className='absolute right-0 top-0 2xl:w-7/12 xl:w-9/12 md:w-11/12 w-10/12 md:visible invisible' />
+      <img src={background} className='absolute right-0 top-0 2xl:w-7/12 xl:w-9/12 md:w-11/12 w-10/12 md:visible invisible' />
       <Navigation></Navigation>
       <Baner></Baner>
       <Gallery images={galleryImages}></Gallery>
